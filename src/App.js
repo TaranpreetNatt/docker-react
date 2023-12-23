2cef2a95ab59a576214d968501850e7d6d3ab07a
Taranpreet Natt <taranpreetnatt@Taranpreets-MBP.lan>
2023-12-23 00:51:22 -0800

    testing branch deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing Feature!
+          Test Feature Branch!
         </p>
         <a
           className="App-link"
